10be38c3030247ab03b25f25cfb02174e882f3ca
Nicolas <nicolasmartin89@gmail.com>
2025-04-11 10:50:06 -0300

    update layout and metadata; add favicon, improve SEO with metadata, and clean up head elements

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,27 @@
-"use client";
-
+// app/layout.tsx
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Navbar } from "./components/Navbar";
 import { Footer } from "./components/Footer";
-import Head from "next/head";
 import { esES } from "@clerk/localizations";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Galería de Recuerdos - Boda Anto & Seba",
+  description: "Galería de fotos de la boda de Anto y Seba.",
+  keywords: ["boda", "Anto y Seba", "galería de fotos", "recuerdos", "amor"],
+  authors: [{ name: "Anto & Seba" }],
+  icons: {
+    icon: "/favicon.png",
+  },
+  openGraph: {
+    title: "Galería de Recuerdos - Boda Anto & Seba",
+    description: "Revive los momentos especiales de la boda de Anto y Seba.",
+    type: "website",
+    url: "https://boda-anto-seba.vercel.app/",
+    
+  },
+};
 
 export default function RootLayout({
   children,
@@ -16,14 +32,6 @@ export default function RootLayout({
     <ClerkProvider localization={esES}>
       <html lang="es">
         <body className="min-h-screen">
-          <Head>
-            <title>Galería de Recuerdos - Boda Anto & Seba</title>
-            <meta
-              name="Boda Anto & Seba"
-              content="Galería de fotos de la boda de Anto y Seba."
-            />
-            <link rel="icon" type="image/png" href="/favicon.png" />
-          </Head>
           <Navbar />
           <main>{children}</main>
           <Footer />
